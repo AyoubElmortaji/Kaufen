78b09c6c41dd898bcc639a140a157117e754b964
Ayoub Elmortaji <88349298+AyoubElmortaji@users.noreply.github.com>
2023-03-10 21:12:23 +0100

    Add files via upload

---
+++ docs/index.js
@@ -0,0 +1,17 @@
+// js file for projet kaufen !!
+// made by elmortaji ayoub !!
+// js for menu
+
+var MenuItems=document.getElementById("MenuItems");
+MenuItems.style.maxHeight="0px";
+
+function menutoggle(){
+	if(MenuItems.style.maxHeight=="0px"){
+		MenuItems.style.maxHeight="200px";
+		
+	}
+
+	else{
+		MenuItems.style.maxHeight="0px";
+	}
+}
